3a186abd52bd0a490a0687816237d50c8c8a14d2
Michelle <michelledhuang@ucla.edu>
2019-04-10 15:47:10 -0700

    Implement linkedList methods

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (!list.head) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (!list.head) {
+      return -1;
+    }
+    var currentHead = list.head;
+    list.head = currentHead.next;
+    currentHead.next = null;
+    return currentHead.value;
   };
 
   list.contains = function(target) {
+    
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
