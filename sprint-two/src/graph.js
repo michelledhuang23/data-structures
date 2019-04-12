0668ac0c388e97bae8357b5f8335fc56d4f23061
Michelle <michelledhuang@ucla.edu>
2019-04-11 18:31:47 -0700

    Implement graph methods

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,38 +2,67 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return node in this.nodes;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var key in this.nodes) {
+    if (this.hasEdge(key, node)) {
+      this.removeEdge(key, node);
+    }
+  }
+  delete this.nodes[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.nodes[fromNode].includes(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode].push(toNode);
+  this.nodes[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var fromNodeParsed = JSON.parse(fromNode);
+  var index1 = this.nodes[fromNodeParsed].indexOf(toNode);
+  var index2 = this.nodes[toNode].indexOf(fromNodeParsed);
+  if (index1 !== -1 && index2 !== -1) {
+    this.nodes[fromNodeParsed].splice(index1, 1);
+    this.nodes[toNode].splice(index2, 1);
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (key in this.nodes) {
+    cb(key);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ //addNode is O(1) 
+ //contains
+ //removeNode
+ //hasEdge
+ //addEdge
+ //removeEdge
+ //forEachNode
  */
 
 
