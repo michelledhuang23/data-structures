88a5d4cfea4f65c243c42442bfa263ad25eb0d42
Michelle <michelledhuang@ucla.edu>
2019-04-12 11:17:34 -0700

    Implement set methods and time complexities exercise

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ // add has time complexity of O(1)
+ // contains has time complexity of O(1)
+ // remove has time complexity of O(1)
  */
