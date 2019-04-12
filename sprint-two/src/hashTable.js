f7f0086fda74e277d00b7d62b87a99f5838ac9cc
Michelle <michelledhuang@ucla.edu>
2019-04-12 14:14:51 -0700

    Implement hash table methods and answering time complexity questions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,56 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (!this._storage[index]) {
+    this._storage[index] = new Array();
+  } else {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        this._storage[index][i][1] = v;
+        return;
+      }
+    }
+  }
+
+  this._storage[index].push([k, v]);
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index]) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        return this._storage[index][i][1];
+      }
+    }
+  } else {
+    return undefined;
+  }
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index]) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        this._storage[index].splice(i, 1);
+      }
+    }
+  } else {
+    return undefined;
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ // insert (assuming we have a really efficient hash function) is O(1)
+ // retrieve (assuming we have a really efficient hash function) is O(1)
+ // remove (assuming we have a really efficient hash function) is O(1)
  */
 
 
